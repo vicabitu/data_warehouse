e5f39942f780d6c4e605b557923f7cfab8698ed7
Emiliano <emilianodemarcoandrada@gmail.com>
2017-06-16 16:29:59 -0300

    Cambios F1

--- facturacion_I.sql
+++ facturacion_I.sql
@@ -26,32 +26,32 @@ create table productos (
 )
 
 alter table productos add constraint pk_productos primary key (nro_producto);
-alter table productos add constraint fk_categoria foreign key (nro_categoria) references categoria (nro_categoria);
+alter table productos add constraint fk_categoria foreign key (nro_categoria) references categoria (nro_categ);
 
 --------------------------------------------------------------------------------------------
 
 create table categoria (
 
-	nro_categoria integer not null,
+	nro_categ integer not null,
 	descripcion varchar(30)
 
 )
 
-alter table categoria add constraint pk_categoria primary key (nro_categoria);
+alter table categoria add constraint pk_categoria primary key (nro_categ);
 
 -------------------------------------------------------------------------------------------
 
-create table ventas (
+create table venta (
 
-	fecha_venta date,
+	fecha_vta date,
 	nro_factura integer,
 	nro_cliente integer,
 	nombre varchar(30),
-	forma_de_pago varchar(30)
+	forma_pago varchar(30)
 )
 
-alter table add constraint pk_ventas primary key (nro_factura);
-alter table add constraint fk_nro_cliente foreign key (nro_cliente) references clientes (nro_cliente); 
+alter table add constraint pk_venta primary key (nro_factura);
+alter table add constraint fk_nro_cliente foreign key (nro_cliente) references clientes (nro_cliente);
 
 ------------------------------------------------------------------------------------------
 
@@ -60,12 +60,12 @@ create table detalle_venta (
 	nro_factura integer,
 	nro_producto integer,
 	descripcion varchar(30),
-	unidad varchar(10),
-	precio real 
+	unidad integer,
+	precio real
 
 )
 
-
-alter table add constraint pk_ventas primary key (nro_factura, nro_producto);
+alter table add constraint pk_venta primary key (nro_factura, nro_producto);
+alter table add constraint fk_nro_factura foreign key (nro_factura) references venta (nro_factura);
 
 ------------------------------------------------------------------------------------------
