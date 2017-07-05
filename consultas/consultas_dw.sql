fce2086f5ca6bd6c10a5604323ed7320ac544684
Emiliano <emilianodemarcoandrada@gmail.com>
2017-07-05 19:58:52 -0300

    consultas 1 y 4 ok

--- consultas/consultas_dw.sql
+++ consultas/consultas_dw.sql
@@ -1,12 +1,14 @@
 ﻿----------------------------------
 ---------- consulta 1-------------
 
-SELECT v.fecha_venta, t.mes, t.anio, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida
+SELECT t.anio, t.mes, p.nombre as producto, c.nombre as cliente, ci.descripcion as ciudad, mp.descripcion as forma_de_pago, sum(v.monto_vendido) as monto, sum(v.cantidad_vendida) as cantidad
 FROM venta v, tiempo t, sucursal s, ciudad ci, provincia prov, region r, clientes c, medio_de_pago mp, producto p
 WHERE t.id_tiempo = v.id_tiempo AND c.id_cliente = v.id_cliente AND s.id_sucursal = v.id_sucursal AND ci.id_ciudad = s.id_ciudad
       AND prov.id_provincia = ci.id_provincia AND r.id_region = prov.id_region AND c.id_cliente = v.id_cliente
       AND mp.id_medio_de_pago = v.id_medio_de_pago AND p.id_producto = v.id_producto
-GROUP BY CUBE (v.fecha_venta, t.mes, t.anio, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida);
+GROUP BY ROLLUP(t.anio, t.mes), CUBE (p.nombre, c.nombre, ci.descripcion, mp.descripcion)
+ORDER BY t.anio, t.mes, p.nombre, c.nombre, ci.descripcion, mp.descripcion;
+--rollup año mes -- cube resto
 
 ----------------------------------
 ---------- consulta 2-------------
@@ -17,33 +19,8 @@ GROUP BY CUBE (v.fecha_venta, t.mes, t.anio, c.nombre, p.nombre, s.direccion, ci
 ----------------------------------
 ---------- consulta 4-------------
 
---SELECT v.fecha_venta, t.mes, t.anio, t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida
---FROM venta v, tiempo t, sucursal s, ciudad ci, provincia prov, region r, clientes c, medio_de_pago mp, producto p
---WHERE t.id_tiempo = v.id_tiempo AND c.id_cliente = v.id_cliente AND s.id_sucursal = v.id_sucursal AND ci.id_ciudad = s.id_ciudad
---      AND prov.id_provincia = ci.id_provincia AND r.id_region = prov.id_region AND c.id_cliente = v.id_cliente
---      AND mp.id_medio_de_pago = v.id_medio_de_pago AND p.id_producto = v.id_producto
---GROUP BY GROUPING SETS ((c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida, v.fecha_venta), 
---			(c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida, t.mes), 
---			(c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida, t.anio), 
---			(c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida, t.trimestre));
-
---SELECT v.fecha_venta, t.mes, t.anio, t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida
---FROM venta v, tiempo t, sucursal s, ciudad ci, provincia prov, region r, clientes c, medio_de_pago mp, producto p
---WHERE t.id_tiempo = v.id_tiempo AND c.id_cliente = v.id_cliente AND s.id_sucursal = v.id_sucursal AND ci.id_ciudad = s.id_ciudad
---      AND prov.id_provincia = ci.id_provincia AND r.id_region = prov.id_region AND c.id_cliente = v.id_cliente
---     AND mp.id_medio_de_pago = v.id_medio_de_pago AND p.id_producto = v.id_producto
---GROUP BY GROUPING SETS ((v.fecha_venta, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida), 
---			(t.mes, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida), 
---			(t.anio, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida), 
---			(t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida));
-
-SELECT v.fecha_venta, t.mes, t.anio, t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida
-FROM venta v, tiempo t, sucursal s, ciudad ci, provincia prov, region r, clientes c, medio_de_pago mp, producto p
-WHERE t.id_tiempo = v.id_tiempo AND c.id_cliente = v.id_cliente AND s.id_sucursal = v.id_sucursal AND ci.id_ciudad = s.id_ciudad
-      AND prov.id_provincia = ci.id_provincia AND r.id_region = prov.id_region AND c.id_cliente = v.id_cliente
-      AND mp.id_medio_de_pago = v.id_medio_de_pago AND p.id_producto = v.id_producto
-GROUP BY GROUPING SETS ((v.fecha_venta, t.mes, t.anio, t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida), 
-			(t.mes, v.fecha_venta, t.anio, t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida), 
-			(t.anio, v.fecha_venta, t.mes, t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida), 
-			(t.trimestre, v.fecha_venta, t.mes, t.anio, t.trimestre, c.nombre, p.nombre, s.direccion, ci.descripcion, prov.descripcion, r.descripcion, mp.descripcion, v.monto_vendido, v.cantidad_vendida));
-
+SELECT t.anio, t.trimestre, t.mes, v.fecha_venta, sum(v.monto_vendido) as monto
+FROM venta v, tiempo t
+WHERE t.id_tiempo = v.id_tiempo
+GROUP BY ROLLUP (t.anio, t.trimestre, t.mes, v.fecha_venta)
+ORDER BY t.anio, t.trimestre, t.mes, v.fecha_venta;
